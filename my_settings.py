ab522364f0ba552266353d5264bc022df67385d5
chanyoung <cyl0504@gmail.com>
2021-12-09 16:03:54 +0900

    first commit

---
+++ my_settings.py
@@ -0,0 +1,13 @@
+DATABASES = {
+    'default' : {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'wecode_28',
+        'USER': 'root',
+        'PASSWORD': '92797506',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+				'OPTIONS': {'charset': 'utf8mb4'}
+    }
+}
+
+SECRET_KEY = 'django-insecure-&baj&r6499x0_e9j05@q!zntk)3-g$_m9m5^9t9i@jvw)jn^vz'
